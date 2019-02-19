7f668f8332329bde8cb93b07bf55ebca382eb852
Amal <amalsouheil@gmail.com>
2019-02-19 11:30:43 +0100

    Perfectionnez votre code grâce la programmation réactive et RxJava

--- app/src/main/java/amal/souheil/netapp/Fragments/MainFragment.java
+++ app/src/main/java/amal/souheil/netapp/Fragments/MainFragment.java
@@ -4,6 +4,7 @@ package amal.souheil.netapp.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,10 @@ import amal.souheil.netapp.Utils.NetworkAsyncTask;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +34,8 @@ import butterknife.OnClick;
         // FOR DESIGN
         @BindView(R.id.fragment_main_textview)
         TextView textView;
+    // 4 - Declare Subscription
+    private Disposable disposable;
 
         public MainFragment() {
         }
@@ -39,16 +46,89 @@ import butterknife.OnClick;
             ButterKnife.bind(this, view);
             return view;
         }
+        @Override
+        public void onDestroy() {
+        super.onDestroy();
+        this.disposeWhenDestroy();
+    }
 
-        // -----------------
+    // -----------------
         // ACTIONS
         // -----------------
 
         @OnClick(R.id.fragment_main_button)
         public void submit(View view) {
             //this.executeHttpRequest();
-            this.executeHttpRequestWithRetrofit();
+            //this.executeHttpRequestWithRetrofit();
+            this.streamShowString();
         }
+
+    // ------------------------------
+    //  Reactive X
+    // ------------------------------
+
+    private void streamShowString(){
+        this.disposable = this.getObservable()
+                .map(getFunctionUpperCase())
+                .flatMap(getSecondObservable()) // 2 - Adding Observable
+                .subscribeWith(getSubscriber());
+    }
+
+
+
+    // 1 - Create a function that will calling a new observable
+    private Function<String,String> getFunctionUpperCase(){
+        return new Function<String,String>() {
+            @Override
+            public String apply(String s) throws Exception {
+                return s.toUpperCase();
+            }
+        };
+    }
+
+    // 1 - Create a function that will calling a new observable
+    private Function<String, Observable<String>> getSecondObservable(){
+        return new Function<String, Observable<String>>() {
+            @Override
+            public Observable<String> apply(String previousString) throws Exception {
+                return Observable.just(previousString+" I love Openclassrooms !");
+            }
+        };
+    }
+
+
+    // 1 - Create Observable
+    private Observable<String> getObservable(){
+        return Observable.just("Cool !");
+    }
+    // 2 - Create Subscriber
+    private DisposableObserver<String> getSubscriber(){
+        return new DisposableObserver<String>() {
+            @Override
+            public void onNext(String item) {
+                textView.setText("Observable emits : "+item);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("TAG","On Error"+Log.getStackTraceString(e));
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("TAG","On Complete !!");
+            }
+        };
+    }
+
+
+
+    // 5 - Dispose subscription
+    private void disposeWhenDestroy(){
+        if (this.disposable != null && !this.disposable.isDisposed()) this.disposable.dispose();
+    }
+
+
 
     // ----------------------------
     //  HTTP REQUEST (Retrofit Way)
